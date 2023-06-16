82b16e2a05f49ff3a840e4ed1b05b2ffed5cee3d
Mikhail <mu.korepanov@gmail.com>
2023-06-16 20:26:28 +0400

    Add state text

--- src/main/java/com/jerseybot/adapters/JDAStateListenerAdapter.java
+++ src/main/java/com/jerseybot/adapters/JDAStateListenerAdapter.java
@@ -1,6 +1,8 @@
 package com.jerseybot.adapters;
 
 import com.jerseybot.JDAStorage;
+import com.jerseybot.config.BotConfig;
+import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
@@ -10,14 +12,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class JDAStateListenerAdapter extends ListenerAdapter {
     private final JDAStorage jdaStorage;
+    private final BotConfig botConfig;
 
     @Autowired
-    public JDAStateListenerAdapter(JDAStorage jdaStorage) {
+    public JDAStateListenerAdapter(JDAStorage jdaStorage, BotConfig botConfig) {
         this.jdaStorage = jdaStorage;
+        this.botConfig = botConfig;
     }
 
     @Override
     public void onReady(@NotNull ReadyEvent event) {
         this.jdaStorage.setJda(event.getJDA());
+        event.getJDA().getPresence().setActivity(Activity.competing(botConfig.getDefaultPrefix() + "help"));
     }
 }
